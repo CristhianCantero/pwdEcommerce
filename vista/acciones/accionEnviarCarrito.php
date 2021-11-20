f1c857d08e465b6a60197161bb97bdc5bf9b6e2e
Cristhian Cantero <cristhian.cantero@init-dev.com.ar>
2021-11-20 00:54:44 -0300

    Cambios carrito y detalles

--- vista/acciones/accionEnviarCarrito.php
+++ vista/acciones/accionEnviarCarrito.php
@@ -2,10 +2,37 @@
 include_once "../../configuracion.php";
 
 $datos = data_submitted();
+$exito = false;
 $abmCompraEstado = new AbmCompraEstado();
+$abmCompraItem = new AbmCompraItem();
+$abmProducto = new AbmProducto();
 
-$arrayCarrito = ['idcompra' => $datos['idcompraitem'], 'idcompraestadotipo' => 1];
-$exito = $abmCompraEstado->alta($arrayCarrito);
+$listadoItems = $abmCompraItem->buscar(['idcompra'=>$datos['idcompraitem']]);
+$stockDisponible = true;
+foreach ($listadoItems as $item) {
+    $respStock = $abmProducto->chequearStock($item);
+    if(!$respStock){
+        $stockDisponible = false;
+    }
+}
+$exito = true;
+if($stockDisponible){
+    $arrayCarrito = ['idcompra' => $datos['idcompraitem'], 'idcompraestadotipo' => 1];
+    $exitoAltaCarrito = $abmCompraEstado->alta($arrayCarrito);
+    if($exitoAltaCarrito){
+        foreach ($listadoItems as $item) {
+            $objProducto = new Producto();
+            $producto = $objProducto->listar("idproducto ='" . $item->getIdProducto()->getIdProducto() . "'");
+            $stockActual = $producto[0]->getProCantStock();
+            $stockActualizado = $stockActual - $item->getCiCantidad();
+            $producto[0]->setProStock($stockActualizado);
+            $respModificar = $producto[0]->modificar();
+            if(!$respModificar){
+                $exito = false;
+            }
+        }
+    }
+}
 
 if ($exito) {
     $message = 'Se envio el carrito correctamente';
